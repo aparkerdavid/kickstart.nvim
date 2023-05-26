7edf3774f0d9e2efae571e282fb2ecb6e14f18fc
Parker Rueve <parkerrueve@gmail.com>
2023-05-26 18:55:03 -0400

    remove lsp commentary from init

--- init.lua
+++ init.lua
@@ -84,18 +84,7 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
