bc41f432a550484e39d080f03e8ec7c1e49c2095
Parker Rueve <parkerrueve@gmail.com>
2023-05-27 17:26:16 -0400

    combine fzf and telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,16 @@
 return {
 	'nvim-telescope/telescope.nvim',
 	branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{
+			'nvim-telescope/telescope-fzf-native.nvim',
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		}
+	},
 	config = function()
 		-- [[ Configure Telescope ]]
 		require('telescope').setup {
