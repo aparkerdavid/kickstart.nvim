0575b8fc28b68f155ff80dd4cc06481ab02a0541
Parker Rueve <parkerrueve@gmail.com>
2023-05-27 19:27:21 -0400

    improve neogit setup

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,11 +1,18 @@
 return {
   'TimUntersberger/neogit',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'sindrets/diffview.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
   config = function()
     local neogit = require("neogit")
     neogit.setup {
       kind = 'vsplit',
       disable_commit_confirmation = true,
+      integrations = {
+        diffview = true,
+      },
     }
   end
 }
