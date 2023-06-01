da1e895210c40d5bbd298463ef89accba7b6c8ca
Parker Rueve <parkerrueve@gmail.com>
2023-05-31 23:38:15 -0400

    use zenwritten for dark mode

--- lua/plugins/auto-dark-mode.lua
+++ lua/plugins/auto-dark-mode.lua
@@ -7,6 +7,8 @@ return {
 			update_interval = 1000,
 			set_dark_mode = function()
 				vim.api.nvim_set_option('background', 'dark')
+				vim.cmd('colorscheme zenwritten')
+				require('lualine').setup { options = { theme = 'zenwritten' } }
 			end,
 			set_light_mode = function()
 				vim.api.nvim_set_option('background', 'light')
