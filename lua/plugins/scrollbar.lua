283db431fcf05569b37d9e68f5f9b1e890953d18
Parker Rueve <parkerrueve@gmail.com>
2023-06-04 22:40:54 -0400

    clean up scrollbar config

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -3,13 +3,12 @@ return {
 	dependencies = { 'lewis6991/gitsigns.nvim' },
 	config = function()
 		require("scrollbar").setup({
+			handle = {
+				blend = 0, -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
+			},
 			handlers = {
 				cursor = false,
-				diagnostic = true,
-				gitsigns = true, -- Requires gitsigns
-				handle = true,
-				search = false,  -- Requires hlslens
-				ale = false,     -- Requires ALE
+				gitsigns = true,
 			},
 		})
 	end
