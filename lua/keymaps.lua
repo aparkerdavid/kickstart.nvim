0e1003e5233a3e283765fac8c172ed424d90811f
Parker Rueve <parkerrueve@gmail.com>
2023-06-10 16:40:12 -0400

    fugitive instead of neogit

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -61,7 +61,7 @@ keys.register(
     },
     g = {
       name = 'git',
-      g = { cmd 'Neogit', 'git status' },
+      g = { cmd 'vert Git', 'git status' },
       c = { cmd 'Git commit', 'commit' },
       s = { gitsigns.stage_hunk, 'stage' },
       r = { gitsigns.reset_hunk, 'reset hunk' },
