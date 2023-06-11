e8a9b9c38dedfe41d56771dd3b417db95f594edd
Parker Rueve <parkerrueve@gmail.com>
2023-06-11 16:59:52 -0400

    refactor file functions

--- lua/files.lua
+++ lua/files.lua
@@ -1,17 +1,12 @@
-local util = require('util')
 local telescope = require('telescope.builtin')
+local util = require('util')
 local files = {}
 
-function files.manage()
-  util.command('e ' .. util.get_buffer_dir())
-end
-
-function files.git()
-  telescope.git_files()
-end
-
-function files.find()
-  telescope.find_files()
+files.git = telescope.git_files
+files.find = telescope.find_files
+files.manage = function()
+  local buffer_dir = util.get_buffer_dir()
+  vim.cmd.e(buffer_dir)
 end
 
 return files
