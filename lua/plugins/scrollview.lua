2aeec8647b85a4a4885ace52392b2aaa9b8b36d2
Parker Rueve <parkerrueve@gmail.com>
2023-06-11 17:00:09 -0400

    configure scrollview

--- lua/plugins/scrollview.lua
+++ lua/plugins/scrollview.lua
@@ -1,1 +1,9 @@
-return { 'dstein64/nvim-scrollview' }
+return {
+	'dstein64/nvim-scrollview',
+	config = function()
+		require('scrollview').setup({
+			include_end_region = true,
+			column = 1,
+		})
+	end
+}
