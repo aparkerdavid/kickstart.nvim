520f0be96964f97079a3025f14f76c7c7c2357bb
Parker Rueve <parkerrueve@gmail.com>
2023-06-19 21:15:13 -0400

    funkyfinder finds files

--- lua/files.lua
+++ lua/files.lua
@@ -1,9 +1,10 @@
 local telescope = require('telescope.builtin')
+local funkyfinder = require('funkyfinder')
 local util = require('util')
 local files = {}
 
 files.git = telescope.git_files
-files.find = telescope.find_files
+files.find = funkyfinder.find_file
 files.manage = function()
   local buffer_dir = util.get_buffer_dir()
   vim.cmd.e(buffer_dir)
