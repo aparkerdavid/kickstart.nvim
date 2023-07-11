1b1879ad314fcf586bd04433f88bde05931447f4
Parker Rueve <parkerrueve@gmail.com>
2023-07-11 19:33:27 -0400

    mini files

--- lua/files.lua
+++ lua/files.lua
@@ -1,5 +1,6 @@
 local telescope = require('telescope.builtin')
 local funkyfinder = require('funkyfinder')
+local mini_files = require 'mini.files'
 local util = require('util')
 local files = {}
 
@@ -7,7 +8,7 @@ files.git = telescope.git_files
 files.find = funkyfinder.find_file
 files.manage = function()
   local buffer_dir = util.get_buffer_dir()
-  vim.cmd.e(buffer_dir)
+  mini_files.open(buffer_dir)
 end
 
 return files
